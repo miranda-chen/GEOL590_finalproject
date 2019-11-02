b06e64824051653ebeb6f4b66dfe4ec1b070a0e2
miranda-chen <mchen31@vosl.utk.edu>
2019-11-02 19:27:18 -0400

    added template of a function and annotated the existing code that reverse scores just one data.frame

--- code_teachinganxiety.R
+++ code_teachinganxiety.R
@@ -47,6 +47,7 @@ library(githubinstall)
 githubinstall("lavaan") 
 library(psych)
 library(semPlot)
+library(tidyverse)
 
 #load data
 getwd() 
@@ -64,14 +65,45 @@ colnames(data)
 
 #Anxiety
 #reverse scoring
-which(colnames(data)=="Q18_10")
-cols = data[,c(39,42,44:45,47:48,51:52,55,58,60,62:63,66)] #check before revser scoring
+which(colnames(data)=="Q18_10") #checking which column names are linked to which column number
+cols = data[,c(39,42,44:45,47:48,51:52,55,58,60,62:63,66)] #choosing the columns which need to be reverse scored
 cols = c("Q18_1", "Q18_4", "Q18_6", "Q18_7", "Q18_9", "Q18_10", "Q19_13", "Q19_14", "Q19_17", "Q19_20", "Q20_22", "Q20_24", "Q20_25","Q20_28")
-data[ ,cols] = 6 - data[ ,cols]
-cols = data[,c(39,42,44:45,47:48,51:52,55,58,60,62:63,66)] #check after scoring
+data[ ,cols] = 6 - data[ ,cols] # subract by the 
+cols = data[,c(39,42,44:45,47:48,51:52,55,58,60,62:63,66)] #check after scoring to ensure they are reversed scored
 
 data["AnxietyScore"] <- NA # That creates the new column named "MY_NEW_COLUMN" filled with "NA"
 data$AnxietyScore <- rowSums(data[,39:67], na.rm=T)  # As an example, the new column receives the result of C - D
-#coping scores
-which(colnames(data)=="Q21_9")
-which(colnames(data)=="Q22_18")
+
+### Draft functions for anxiety filtering numeric, NOT blank, survey data###
+
+teach_anxiety <- function(x, index.1, index.2) {
+  
+  col1 <- x[, index.1]
+  col2 <- x[, index.2]
+  
+  # test whether x is logical 
+  if(is.data.frame(x) == F) {
+    warning("This is not a data frame ") #pass a message when it's okay, warning if something is wrong
+    new_x <- NA
+  } 
+  
+  if(is.numeric(col1) == F & (is.numeric(col2) == F)) {
+    warning("Columns are not numeric ") #pass a message when it's okay, warning if something is wrong
+    new_x <- NA 
+    
+  } else {
+    
+    new_x <- mutate(x, sum.col =  col1 +  col2) #adding two of those indexed columns
+  }
+  #last statement is what the function returns
+  print(new_x)
+} 
+
+# need to use 
+read.csv()
+
+is.blank()
+
+
+
+
